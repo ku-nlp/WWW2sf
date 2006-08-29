fa0d91c99026b879914da89c72c6dd6974889ac5
Tomohide Shibata <shibata@i.kyoto-u.ac.jp>
2006-08-29 02:33:22 +0000

    added.

---
+++ tool/perl/SentenceFilter.pm
@@ -0,0 +1,39 @@
+package SentenceFilter;
+
+# Mark Japanese sentences that are judged as Japanese
+
+# $Id$
+
+use utf8;
+use strict;
+
+sub new {
+    my ($this) = @_;
+
+    $this = {};
+
+    bless $this;
+    return $this;
+}
+
+sub JapaneseCheck {
+    my ($this, $buf) = @_;
+    my ($acount, $count);
+
+    unless (utf8::is_utf8($buf)) {
+	require Encode;
+	$buf = Encode::decode('utf8', $buf)
+    }
+
+    for my $str (split(//, $buf)) {
+	# count Hiragana, Katakana or Kanji
+	if ($str =~ /^\p{Hiragana}|\p{Katakana}|ー|\p{Han}$/) {
+	    $count++;
+	}
+	$acount++;
+    }
+    return 0 unless $acount;
+    return $count / $acount;
+}
+
+1;
