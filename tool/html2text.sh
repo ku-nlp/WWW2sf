498ae81c67a4238c64c12b7657c4b60dd5043b5e
Daisuke Kawahara <dk@i.kyoto-u.ac.jp>
2007-02-20 14:27:08 +0000

    オプションを追加

--- tool/html2text.sh
+++ tool/html2text.sh
@@ -7,13 +7,33 @@ usage() {
     exit 1
 }
 
+extract_std_args="--checkzyoshi --checkjapanese --checkencoding"
+extract_args=
+formatwww_args=
+
+while getopts bfph OPT
+do  
+    case $OPT in
+	b)  extract_args="--ignore_br $extract_args"
+	    ;;
+	f)  extract_std_args=""
+	    ;;
+        p)  formatwww_args="--include_paren $formatwww_args"
+            ;;
+        h)  usage
+            ;;
+    esac
+done
+shift `expr $OPTIND - 1`
+
 if [ ! -f "$1" ]; then
     usage
 fi
 
-head=`basename $1 .html`
-tmpfile=$head.$$
+f=$1
+base_f=`expr $f : "\(.*\)\.[^\.]*$"`
+tmpfile=$base_f.$$
 
-perl -I perl scripts/extract-sentences.perl --checkzyoshi --checkjapanese --checkencoding $1 | perl -I perl scripts/sentence-filter.perl > $tmpfile
-perl -I perl scripts/format-www.perl $tmpfile
+perl -I perl scripts/extract-sentences.perl $extract_std_args $extract_args $f | perl -I perl scripts/sentence-filter.perl > $tmpfile
+perl -I perl scripts/format-www.perl $formatwww_args $tmpfile
 rm -f $tmpfile
