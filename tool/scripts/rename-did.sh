ff401b1b4b662e35d527dd0e5027631eaada7de0
Keiji Shinzato <skeiji@(null)>
2008-03-24 11:09:52 +0000

    ウェブページ、utf8化されたウェブページについてもIDを振りなおすように修正．

--- tool/scripts/rename-did.sh
+++ tool/scripts/rename-did.sh
@@ -16,13 +16,25 @@ offset=$1 ; shift
 
 find $@ -type f | sort > $workdir/find.files.$$
 
-for d in `cat $workdir/find.files.$$ | awk '{printf("'$distdir'/x%05d\n", ('$offset' + NR - 1)/ 10000)}' | sort -u`
+for d in `cat $workdir/find.files.$$ | awk '{printf("%05d\n", ('$offset' + NR - 1)/ 10000)}' | sort -u`
 do
-    echo mkdir $d >> move.$$
+   mkdir $distdir/h$d
+   mkdir $distdir/x$d
+   mkdir $distdir/u$d
 done
 
-find $@ -type f | sort | awk '{printf("mv %s '$distdir'/x%05d/%09d.xml.gz\n", $1, ('$offset' + NR - 1)/ 10000, NR -1 + '$offset')}' >> move.$$
+find $@ -type f | sort | perl -pe 's/\.[^\.]+$/\n/' | perl -pe 's/^x//' | awk '{printf("%s %05d/%09d\n", $1, ('$offset' + NR - 1)/ 10000, NR -1 + '$offset')}' > ids.map.$$
 
-sh move.$$
-rm move.$$
+cat ids.map.$$ | awk '{printf("mv h%s.html '$distdir'/h%s.html\n", $1, $2)}' > move.h.$$
+cat ids.map.$$ | awk '{printf("mv x%s.xml  '$distdir'/x%s.xml\n",  $1, $2)}' > move.x.$$
+cat ids.map.$$ | awk '{printf("mv h%s.*utf8.html '$distdir'/u%s.utf8.html\n", $1, $2)}' > move.u.$$
+
+sh move.h.$$
+sh move.x.$$
+sh move.u.$$
+rm move.h.$$
+rm move.x.$$
+rm move.u.$$
+
+rm ids.map.$$
 rm $workdir/find.files.$$
