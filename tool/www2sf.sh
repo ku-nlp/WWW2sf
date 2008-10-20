7bc89da4e53507bd79c209fafd27f57421a51d42
Daisuke Kawahara <dk@i.kyoto-u.ac.jp>
2008-10-20 09:31:54 +0000

    Created -z option for gzipped htmls.

--- tool/www2sf.sh
+++ tool/www2sf.sh
@@ -10,13 +10,14 @@ usage() {
 }
 
 html2sf_extra_args=
+ext=
 
 # ファイルサイズの閾値(default 5M)
 fsize_threshold=5242880
 
 base_dir=`dirname $0`
 
-while getopts jkshS:c:uU OPT
+while getopts jkshS:c:uUz OPT
 do  
     case $OPT in
 	j)  html2sf_extra_args="-j"
@@ -33,6 +34,8 @@ do
 	    ;;
 	U)  html2sf_extra_args="-U $html2sf_extra_args"
 	    ;;
+	z)  ext=".gz"
+	    ;;
         h)  usage
             ;;
     esac
@@ -46,9 +49,17 @@ if [ ! -d $xdir ]; then
     mkdir -p $xdir
 fi
 
-for f in $hdir/*.html
+for in_f in $hdir/*.html$ext
 do
-    base_f=`basename $f .html`
+    f=$in_f
+    base_f=`basename $in_f .html$ext`
+
+    # -z(gzip圧縮)オプションが指定されたとき
+    if [ "$ext" = ".gz" ]; then
+	gzip -dc $f > $xdir/$base_f.html
+	f=$xdir/$base_f.html
+    fi
+
     fsize=`wc -c $f | awk '{print $1}'`
     # ファイルサイズが$fsize_threshold以下なら
     if [ $fsize -lt $fsize_threshold ]; then
@@ -59,5 +70,9 @@ do
 	if [ ! -s $xdir/$base_f.xml ]; then
             rm -f $xdir/$base_f.xml
 	fi
+    fi
+
+    if [ "$ext" = ".gz" ]; then
+	rm -f $f
     fi
 done
