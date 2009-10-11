b1288718f58e7ebac5592eb0073e41f29fa7a14c
Keiji Shinzato <skeiji@(null)>
2009-10-11 13:29:19 +0000

    出力結果を特定のホストにコピーするように.

--- tool/scripts/make-standard-format.sh
+++ tool/scripts/make-standard-format.sh
@@ -1,33 +1,44 @@
 #!/bin/sh
 
+# $Id$
+
 # ディレクトリ中のHTML文書を標準フォーマットに変換するスクリプト
 
 # ★以下の変数の値を変更すること
-workspace=/data/home/skeiji/mksf_test
-scriptdir=/share19/work/skeiji/test/WWW2sf/tool
+workspace=/data/skeiji/mksf
+# このスクリプトの一つ上
+# scriptdir=`echo $PWD/$0 | xargs dirname | xargs dirname`
+scriptdir=/home/skeiji/ipsj/WWW2sf/tool
+# オプション
+CNDB=/home/skeiji/public_html/data/cns.cdb
+OPTION="-U -c $CNDB"
 
 
+. $HOME/.zshrc
 
 fp=$1;
+distdir=$2
 fname=`basename $fp`
-fid=`echo $fname | cut -f 2 -d 'h'`
+fid=`echo $fname | cut -f 1 -d . | cut -f 2 -d 'h'`
 
 hdir=h$fid
 xdir=x$fid
 
 if [ ! -e $workspace ]; then
-    mkdir $workspace
+    mkdir -p $workspace
 fi
 
 cd $workspace
-echo cp -r $fp ./
-cp -r $fp ./
+scp $fp ./
+tar xzf $fname
+rm $fname
 
 echo mkdir $workspace/$xdir
 mkdir $workspace/$xdir
 
-echo sh $scriptdir/www2sf.sh -k $hdir $xdir
-sh $scriptdir/www2sf.sh -k $hdir $xdir
+cd $workspace
+echo sh $scriptdir/www2sf.sh $hdir $xdir
+sh $scriptdir/www2sf.sh $OPTION $hdir $xdir
 
 echo rm -r $hdir
 rm -r $hdir
@@ -41,3 +52,6 @@ tar czf $xdir.tgz $xdir
 
 echo rm -r $xdir
 rm -r $xdir
+
+scp $xdir.tgz $distdir
+rm $xdir.tgz
