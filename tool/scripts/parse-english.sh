5c1d677ff1fe25d4af57cde88988ab36446e1082
Daisuke Kawahara <dk@i.kyoto-u.ac.jp>
2010-03-02 03:45:14 +0000

    Modified to use MaltParser.

--- tool/scripts/parse-english.sh
+++ tool/scripts/parse-english.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-# parse English sentences using tagger and MSTParser
+# parse English sentences using a POS tagger and a dependency parser
 
-JdkDir=$HOME/share/tool/jdk1.6.0_04
+TMPDir=/data/kawahara/parse-english-$$
+JdkDir=$HOME/share/tool/jdk1.6.0_17
 TaggerDir=$HOME/share/tool/postagger-1.0
-ParserDir=$HOME/share/tool/mstparser
+ParserDir=$HOME/share/tool/malt-1.3.1
+ModelFile=$HOME/share/tool/malt-1.3.1/engmalt.mco
 OrigDir=`pwd`
 ScriptsDir=`dirname $0`
-
-JavaHeapMem=3600m
+JavaHeapMem=1024m
 export PATH=$JdkDir/bin:$PATH
 
 usage() {
@@ -16,8 +17,25 @@ usage() {
     exit 1
 }
 
-cd $TaggerDir
+if [ -f "$OrigDir/$1" ]; then
+    InputFile="$OrigDir/$1"
+elif [ -f "$1" ]; then
+    InputFile=$1
+else
+    usage
+fi
+InputFileName=`basename $InputFile`
+ModelFileName=`basename $ModelFile`
 
+if [ ! -d "$TMPDir" ]; then
+    mkdir -p $TMPDir
+    if [ ! -d "$TMPDir" ]; then
+	echo "Cannot create TMPDir ($TMPDir)!"
+	exit 1
+    fi
+fi
+
+cd $TaggerDir
 if [ ! -d "$ScriptsDir" ]; then
     ScriptsDir=$OrigDir/$ScriptsDir
     if [ ! -d "$ScriptsDir" ]; then
@@ -26,11 +44,16 @@ if [ ! -d "$ScriptsDir" ]; then
     fi
 fi
 
-grep -v '^#' $OrigDir/$1 > $1.$$.raw.txt
-./tagger < $1.$$.raw.txt 2> /dev/null | perl $ScriptsDir/tagger-out2conll.perl > $1.$$.tagged.conll
-java -classpath "$ParserDir:$ParserDir/lib/trove.jar" -Xmx$JavaHeapMem mstparser.DependencyParser test test-file:$1.$$.tagged.conll model-name:$ParserDir/model/ptb-o2.model order:2 output-file:$1.$$.parsed.conll > /dev/null
-perl $ScriptsDir/lemmatize-conll.perl $1.$$.parsed.conll > $1.$$.parsed.lem.conll
-perl $ScriptsDir/add-sid-conll.perl $OrigDir/$1 $1.$$.parsed.lem.conll > $1.$$.parsed.sid.conll
-cat $1.$$.parsed.sid.conll
+grep -v '^#' $InputFile > $TMPDir/$InputFileName.$$.raw.txt
+./tagger < $TMPDir/$InputFileName.$$.raw.txt 2> /dev/null | perl $ScriptsDir/tagger-out2conll.perl > $TMPDir/$InputFileName.$$.tagged.conll
 
-rm -f $1.$$.raw.txt $1.$$.tagged.conll $1.$$.parsed.conll $1.$$.parsed.lem.conll $1.$$.parsed.sid.conll
+cd $TMPDir
+
+cp $ModelFile .
+java -Xmx$JavaHeapMem -jar $ParserDir/malt.jar -c engmalt -m parse < $TMPDir/$InputFileName.$$.tagged.conll > $TMPDir/$InputFileName.$$.parsed.conll 2> /dev/null
+perl $ScriptsDir/lemmatize-conll.perl $TMPDir/$InputFileName.$$.parsed.conll > $TMPDir/$InputFileName.$$.parsed.lem.conll
+perl $ScriptsDir/add-sid-conll.perl $InputFile $TMPDir/$InputFileName.$$.parsed.lem.conll > $TMPDir/$InputFileName.$$.parsed.sid.conll
+cat $TMPDir/$InputFileName.$$.parsed.sid.conll
+
+rm -f $TMPDir/$ModelFileName $TMPDir/$InputFileName.$$.raw.txt $TMPDir/$InputFileName.$$.tagged.conll $TMPDir/$InputFileName.$$.parsed.conll $TMPDir/$InputFileName.$$.parsed.lem.conll $TMPDir/$InputFileName.$$.parsed.sid.conll
+rm -rf $TMPDir
