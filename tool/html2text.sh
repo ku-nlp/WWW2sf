a14cf84294fb5fbe29aaf02dad5d2218eabdaff5
Daisuke Kawahara <dk@i.kyoto-u.ac.jp>
2011-07-11 06:34:57 +0000

    Adapted the change of format-www.perl.

--- tool/html2text.sh
+++ tool/html2text.sh
@@ -9,7 +9,7 @@ usage() {
 
 # -b: <br>と<p>を無視 (extract-sentences.perl --ignore_br)
 # -B: Movable Type用オプション (extract-sentences.perl --blog mt)
-# -f: 日本語チェックをしない (extract-sentences.perl)
+# -f: 日本語チェックをしない (extract-sentences.perl)、エンコーディングをUTF-8として処理
 # -p: 括弧を文内に含める (format-www.perl --inclue_paren)
 # -P: 括弧を文として分ける (format-www.perl --divide_paren)
 # -w: 全体削除しない (format-www.perl --save_all)
@@ -26,6 +26,7 @@ do
 	B)  extract_args="--blog mt $extract_args"
 	    ;;
 	f)  extract_std_args=""
+	    formatwww_args="--encoding utf8 $formatwww_args"
 	    ;;
         p)  formatwww_args="--include_paren $formatwww_args"
             ;;
@@ -49,16 +50,17 @@ fi
 f=$1
 base_dir=`dirname $0`
 base_f=`expr $f : "\(.*\)\.[^\.]*$"`
+formatwww_args="--head $base_f $formatwww_args"
 tmpfile=$base_f.$$
 tmpfile2=${base_f}_2.$$
 trap 'rm -f $tmpfile; exit 1' 1 2 3 15
 
-perl -I $base_dir/perl $base_dir/scripts/extract-sentences.perl $extract_std_args $extract_args $f | nkf -We > $tmpfile
+perl -I $base_dir/perl $base_dir/scripts/extract-sentences.perl $extract_std_args $extract_args $f > $tmpfile
 
 if [ -n "$extract_std_args" ]; then
-    perl -I $base_dir/perl $base_dir/scripts/sentence-filter.perl $tmpfile > $tmpfile2
+    nkf -We $tmpfile | perl -I $base_dir/perl $base_dir/scripts/sentence-filter.perl > $tmpfile2
     mv -f $tmpfile2 $tmpfile
 fi
 
-perl -I $base_dir/perl $base_dir/scripts/format-www.perl $formatwww_args $tmpfile
+perl -I $base_dir/perl $base_dir/scripts/format-www.perl $formatwww_args < $tmpfile
 rm -f $tmpfile
