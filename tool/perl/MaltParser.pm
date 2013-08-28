6e762b9d06de55255fb0471b2d63902bef5936d2
Daisuke Kawahara <dk@i.kyoto-u.ac.jp>
2013-08-28 15:41:55 +0000

    Moved conll2sf() to StandardFormatLib.pm.

--- tool/perl/MaltParser.pm
+++ tool/perl/MaltParser.pm
@@ -61,7 +61,6 @@ sub new {
     $this->{WTR}->autoflush(1);
 
     if ($this->{opt}{output_sf}) { # 解析結果を標準フォーマットにする場合
-	require XML::LibXML;
 	require StandardFormatLib;
     }
 
@@ -92,7 +91,7 @@ sub analyze_from_conll {
     }
 
     if ($this->{opt}{output_sf}) {
-	return $this->conll2sf($buf);
+	return &StandardFormatLib::conll2sf($buf);
     }
     else {
 	return $buf;
@@ -109,43 +108,6 @@ sub analyze {
     return undef unless $tagged_result;
 
     return $this->analyze_from_conll($tagged_result);
-}
-
-# CoNLL形式を標準フォーマットに変換
-sub conll2sf {
-    my ($this, $result) = @_;
-
-    my $doc = XML::LibXML::Document->new('1.0');
-    my $sf_node = $doc->createElement('StandardFormat');
-    my $sentence_node = $doc->createElement('S');
-    $sf_node->appendChild($sentence_node);
-    my $annotation_node = $doc->createElement('Annotation');
-    $sentence_node->appendChild($annotation_node);
-
-    for my $line (split("\n", $result)) {
-	last if $line =~ /^$/;
-	my @line = split(/\t/, $line);
-	my $head = 'c' . $line[6]; # c1, c2, ...
-	$head = 'c-1' if $head eq 'c0'; # ROOT
-	my %pf = (id => 'c' . $line[0], head => $head, type => $line[7]);
-	my %wf = (id => 't' . $line[0], surf => $line[1], orig => $line[2], pos1 => $line[3]);
-
-	my $phrase_node = $doc->createElement('Chunk');
-	for my $key (sort {$StandardFormatLib::pf_order{$a} <=> $StandardFormatLib::pf_order{$b}} keys %pf) {
-	    $phrase_node->setAttribute($key, $pf{$key});
-	}
-
-	my $word_node = $doc->createElement('Token');
-	for my $key (sort {$StandardFormatLib::wf_order{$a} <=> $StandardFormatLib::wf_order{$b}} keys %wf) {
-	    $word_node->setAttribute($key, $wf{$key});
-	}
-
-	$phrase_node->appendChild($word_node);
-	$annotation_node->appendChild($phrase_node);
-    }
-
-    $doc->setDocumentElement($sf_node);
-    return $doc->toString();
 }
 
 1;
--- tool/perl/StandardFormatLib.pm
+++ tool/perl/StandardFormatLib.pm
@@ -2,11 +2,58 @@ package StandardFormatLib;
 
 # Common settings for StandardFormat
 
+use base 'Exporter';
 use strict;
 
+our @EXPORT = qw/conll2sf/;
 our %pf_order = (id => 0, head => 1, category => 2, feature => 3, type => 4); # print order of phrase attributes
 our %wf_order = (id => 0, surf => 1, orig => 2, read => 3, pos1 => 4, repname => 5, conj => 6, feature => 99); # print order of word attributes
 our %synnodesf_order = (head => 0, phraseid => 1);
 our %synnodef_order = (wordid => 0, synid => 1, score => 2);
+
+# CoNLL形式を標準フォーマットに変換
+sub conll2sf {
+    my ($result) = @_;
+
+    require XML::LibXML;
+    my $doc = XML::LibXML::Document->new('1.0');
+    my $sf_node = $doc->createElement('StandardFormat');
+    my $annotation_node = undef;
+
+    for my $line (split("\n", $result)) {
+	if ($line =~ /^$/) {
+	    $annotation_node = undef;
+	    next;
+	}
+	my @line = split(/\t/, $line);
+	my $head = 'c' . $line[6]; # c1, c2, ...
+	$head = 'c-1' if $head eq 'c0'; # ROOT
+	my %pf = (id => 'c' . $line[0], head => $head, type => $line[7]);
+	my %wf = (id => 't' . $line[0], surf => $line[1], orig => $line[2], pos1 => $line[3]);
+
+	my $phrase_node = $doc->createElement('Chunk');
+	for my $key (sort {$pf_order{$a} <=> $pf_order{$b}} keys %pf) {
+	    $phrase_node->setAttribute($key, $pf{$key});
+	}
+
+	my $word_node = $doc->createElement('Token');
+	for my $key (sort {$wf_order{$a} <=> $wf_order{$b}} keys %wf) {
+	    $word_node->setAttribute($key, $wf{$key});
+	}
+
+	if (!defined($annotation_node)) {
+	    my $sentence_node = $doc->createElement('S');
+	    $sf_node->appendChild($sentence_node);
+	    $annotation_node = $doc->createElement('Annotation');
+	    $sentence_node->appendChild($annotation_node);
+	}
+
+	$phrase_node->appendChild($word_node);
+	$annotation_node->appendChild($phrase_node);
+    }
+
+    $doc->setDocumentElement($sf_node);
+    return $doc->toString();
+}
 
 1;
