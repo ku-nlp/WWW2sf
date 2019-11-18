3c10451d0dbdc8b2305e7e8ee338d6fa3ccf7a03
Daisuke Kawahara <dk@i.kyoto-u.ac.jp>
2019-11-18 18:00:54 +0900

    Modified to output text as UTF-8.

--- tool/html2text.sh
+++ tool/html2text.sh
@@ -16,7 +16,7 @@ usage() {
 
 extract_std_args="--checkzyoshi --checkjapanese --checkencoding"
 extract_args=
-formatwww_args=
+formatwww_args="--encoding utf8"
 
 while getopts bfpPhBsw OPT
 do  
@@ -26,7 +26,6 @@ do
 	B)  extract_args="--blog mt $extract_args"
 	    ;;
 	f)  extract_std_args=""
-	    formatwww_args="--encoding utf8 $formatwww_args"
 	    ;;
         p)  formatwww_args="--include_paren $formatwww_args"
             ;;
@@ -58,7 +57,7 @@ trap 'rm -f $tmpfile; exit 1' 1 2 3 15
 perl -I $base_dir/perl $base_dir/scripts/extract-sentences.perl $extract_std_args $extract_args $f > $tmpfile
 
 if [ -n "$extract_std_args" ]; then
-    nkf -We $tmpfile | perl -I $base_dir/perl $base_dir/scripts/sentence-filter.perl > $tmpfile2
+    nkf -We $tmpfile | perl -I $base_dir/perl $base_dir/scripts/sentence-filter.perl | nkf -Ew > $tmpfile2
     mv -f $tmpfile2 $tmpfile
 fi
 
